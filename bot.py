a2bb606d23a5995f93b171c75fb6888c6a963393
WarLikeLaux <warlikelaux080399@mail.ru>
2023-11-11 18:18:03 +0600

    Added feature to publish text to Telegram channel

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+
+import telegram
+from dotenv import load_dotenv
+
+
+def main():
+    load_dotenv()
+    tg_bot_token = os.environ["TG_BOT_TOKEN"]
+    tg_channel_id = os.environ["TG_CHANNEL_ID"]
+    bot = telegram.Bot(token=tg_bot_token)
+    bot.send_message(
+        chat_id=tg_channel_id,
+        text="I'm sorry Dave I'm afraid I can't do that."
+    )
+
+
+if __name__ == "__main__":
+    main()
