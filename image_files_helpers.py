db62994c0c609bf248b6db68d807b5381dc31ca5
WarLikeLaux <warlikelaux080399@mail.ru>
2023-11-12 14:11:18 +0600

    Refactored image_files_helpers.py

--- image_files_helpers.py
+++ image_files_helpers.py
@@ -1,14 +1,27 @@
 import os
 import urllib
 from pathlib import Path
-
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
-IMAGES_DIRECTORY = os.path.join(os.getcwd(), os.environ["IMAGES_DIRECTORY"])
+
+def initialize_module():
+    load_dotenv()
+    global IMAGES_DIRECTORY
+    IMAGES_DIRECTORY = os.path.join(
+        os.getcwd(), os.environ["IMAGES_DIRECTORY"]
+    )
+    Path(IMAGES_DIRECTORY).mkdir(parents=True, exist_ok=True)
 
 
+def module_function_wrapper(func):
+    def wrapper(*args, **kwargs):
+        initialize_module()
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@module_function_wrapper
 def save_image(url, filename, params={}):
     response = requests.get(url, params=params)
     response.raise_for_status()
@@ -16,17 +29,16 @@ def save_image(url, filename, params={}):
         file.write(response.content)
 
 
+@module_function_wrapper
 def get_file_extension(url):
     path = urllib.parse.urlparse(url).path
     return os.path.splitext(path)[1].lower()
 
 
+@module_function_wrapper
 def get_images():
     images_files = []
     for filename in os.listdir(IMAGES_DIRECTORY):
         if get_file_extension(filename) in (".png", ".jpg", ".jpeg"):
             images_files.append(f"{IMAGES_DIRECTORY}/{filename}")
     return images_files
-
-
-Path(IMAGES_DIRECTORY).mkdir(parents=True, exist_ok=True)
